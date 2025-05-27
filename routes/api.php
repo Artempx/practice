11545920a1fc5141045680ea30fece107766cd7b
unknown <artem.kardash@nure.ua>
2025-05-27 16:30:32 +0300

    loginFeature

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController; 
 
-Route::get('/login', [LoginController::class, 'login']);
+Route::post('/login', [LoginController::class, 'login']);
 Route::post('/register', [RegisterController::class, 'register']);
