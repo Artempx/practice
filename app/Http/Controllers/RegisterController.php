df40dfc523b941ddbcf53c2dd264eb7f86e0d606
Artempx <artem.zadorozhnyi@nure.ua>
2025-05-27 15:30:21 +0300

    deleted fields

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,7 +13,6 @@ public function register(Request $request)
     $validator = Validator::make($request->all(), [
         'fullName'     => 'required|string|max:255',
         'email'    => 'required|string|email|max:255|unique:users',
-        'password' => 'required|string|min:6|confirmed',
     ]);
 
     if($validator->fails()){
