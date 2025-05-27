69920216ec123ffa343cf39e6c6db4c5a39bc78b
Artempx <artem.zadorozhnyi@nure.ua>
2025-05-27 23:36:49 +0300

    added route to get all notes

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,10 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\AddNoteController; 
 use App\Http\Controllers\DeleteNoteController; 
 use App\Http\Controllers\EditNoteController; 
-
+use App\Http\Controllers\GetNoteController; 
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/addNote', [AddNoteController::class, 'addNote']);
 Route::delete('/delete', [DeleteNoteController::class, 'deleteNote']);
 Route::patch('/editNote', [EditNoteController::class, 'editNote']);
+Route::get('/note', [GetNoteController::class,'getnote']);
