53a74197f5fe4dc1dbf1b285617d8c90bce263c8
Artempx <artem.zadorozhnyi@nure.ua>
2025-05-29 22:31:14 +0300

    added export and import routes

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,13 @@ use App\Http\Controllers\AddNoteController;
 use App\Http\Controllers\DeleteNoteController; 
 use App\Http\Controllers\EditNoteController; 
 use App\Http\Controllers\GetNoteController; 
+use App\Http\Controllers\SyncController;
+
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/addNote', [AddNoteController::class, 'addNote']);
 Route::delete('/delete', [DeleteNoteController::class, 'deleteNote']);
 Route::patch('/editNote', [EditNoteController::class, 'editNote']);
 Route::get('/note', [GetNoteController::class,'getnote']);
+Route::get('/note/{id}/export', [ExportNoteController::class, 'export']);
+Route::post('/import', [ImportNoteController::class, 'import']);
